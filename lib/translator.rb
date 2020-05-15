c4fa84c08169787e2689627b850689a2a417685e
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-05-15 16:20:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require 'yaml'
 def load_library(file_path)
   # code goes here
   emoticon_library = YAML.load(File.read('./lib/emoticons.yml')) #automatically creates a hash
-  
+
 end
 
 def get_japanese_emoticon
