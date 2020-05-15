5c2d69f1ee4cb6375b0e446f61f51d5fe6f45b0d
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-05-15 17:26:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ def load_library(file_path)
     new_hash[:meaning] = {}
     new_hash[:meaning][:english] = {}
     new_hash[:meaning][:japanese] = {}
-      binding.pry
+    binding.pry
     # new_hash[:english] = emoticon[0]
     # new_hash[:japanese] = emoticon[1]
   # emoticon_library = Hash.new
