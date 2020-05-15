fed1fdf09c125327433a712d333ae5ec80a70856
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-05-15 18:04:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,10 +18,6 @@ end
 def get_english_meaning(file_path, emoticon)
   # code goes here
   new_hash = load_library(file_path)
-  if new_hash[meaning][:japanese]
-    return new_hash[meaning][:japanese]
-  end
-  binding.pry
 end
 
 def get_japanese_emoticon(file_path, emoticon)
