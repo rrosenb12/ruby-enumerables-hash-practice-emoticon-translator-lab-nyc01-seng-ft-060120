08e842e13317ddced9bd5c5b7352c5a0e5281c42
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-05-15 18:27:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,10 +21,9 @@ def get_english_meaning(file_path, emoticon)
   new_hash.keys.find do |meaning|
     if new_hash[meaning][:japanese] == emoticon
       return meaning
-    else
-      
     end
   end
+  puts "Sorry, that emoticon was not found"
 end
 
 def get_japanese_emoticon
