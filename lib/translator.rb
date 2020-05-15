63836fc276eba7ef63bf3ed83c2e16332fd49c28
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-05-15 18:28:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,6 +26,6 @@ def get_english_meaning(file_path, emoticon)
   return "Sorry, that emoticon was not found"
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
   # code goes here
 end
