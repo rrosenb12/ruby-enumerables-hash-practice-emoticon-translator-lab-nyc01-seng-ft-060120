45d53557e954e3b4026651d53e381972cb21feca
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-05-15 17:33:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,6 +7,7 @@ def load_library(file_path)
   # code goes here
   emoticon_library = YAML.load(File.read('./lib/emoticons.yml')) #automatically creates a hash
   new_hash = Hash.new
+  emoticon_library = new_hash
   emoticon_library.each do |meaning, emoticon|
     new_hash[meaning] = {}
     new_hash[meaning][:english] = emoticon[0]
